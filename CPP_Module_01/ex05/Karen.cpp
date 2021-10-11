d51be6584f45a74f1d2d6dc04a6043f84c9eaff5
Ulistonee <ulistonee@gmail.com>
2021-10-11 23:10:36 +0300

    working on ex05

---
+++ CPP_Module_01/ex05/Karen.cpp
@@ -0,0 +1,18 @@
+#include "Karen.hpp"
+
+void 		Karen::debug() {
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n";
+}
+
+void 		Karen::info()
+{
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n";
+}
+
+void 		Karen::warning() {
+	std::cout <<  "I think I deserve to have some extra bacon for free. I’ve bee coming here for years and you just started working here last month.\n";
+}
+
+void 		Karen::error() {
+	std::cout << "This is unacceptable, I want to speak to the manager now.\n";
+}
---
+++ CPP_Module_01/ex05/Karen.hpp
@@ -0,0 +1,21 @@
+#ifndef KAREN_H
+#define KAREN_H
+# include <iostream>
+# include <fstream>
+# include <string>
+# include <cstdlib>
+# include <cstdio>
+
+class Karen
+{
+private:
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+
+public:
+	void complain( std::string level );
+};
+
+#endif
