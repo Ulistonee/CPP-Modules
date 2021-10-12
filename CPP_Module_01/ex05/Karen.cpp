7ac28b833f49b829ceadcb9a69c1b90095b95ff5
Ulistonee <ulistonee@gmail.com>
2021-10-12 22:56:41 +0300

    ex05 is done

--- CPP_Module_01/ex05/Karen.cpp
+++ CPP_Module_01/ex05/Karen.cpp
@@ -16,3 +16,12 @@ void 		Karen::warning() {
 void 		Karen::error() {
 	std::cout << "This is unacceptable, I want to speak to the manager now.\n";
 }
+
+void 		Karen::complain(std::string level) {
+	int 	i = 0;
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Karen::*fcnPtr[])() = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+	while (i < 3 && level != levels[i])
+		i++;
+	(this->*(fcnPtr[i]))();
+}
