16977934fdbdafeb39bc66ad199966a809fe6b9f
Ulistonee <ulistonee@gmail.com>
2021-12-14 10:29:47 +0300

    CPP07

---
+++ CPP_Module_07/ex00/sandbox.C
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <fstream>
+# include <string>
+# include <cstdlib>
+# include <cstdio>
+# include <cmath>
+# include <cstdio>
+# include <stdexcept>
+
+template <typename T>
+
+T const & max(T const & x, T const & y)
+{
+	return (x >= y ? x : y);
+}
+
+
+int 	main()
+{
+
+
+	return 0;
+}
